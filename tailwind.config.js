258a803dfd3616b1a46ec1d6f6997c8c4faa8002
Miwa <miwa.tnk9675@gmail.com>
2022-10-08 21:33:55 +0900

    changes on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  purge: {
+    content: [
+      './src/pages/**/*.{js,ts,jsx,tsx}',
+      './src/components/**/*.{js,ts,jsx,tsx}'
+    ],
+    options: {
+      // https://purgecss.com/safelisting.html#patterns
+      safelist: {
+        standard: [/^bg-/, /^text-/],
+      },
+    },
+  },
   content: [],
   theme: {
     extend: {},
